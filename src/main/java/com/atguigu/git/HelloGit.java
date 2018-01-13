3ab537c10ca34fda3e16a5563c617b689fca806a
root <firebird125@163.com>
2018-01-14 01:07:44 +0800

    jjjjjj

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("aaa...");
+		System.out.println("a...");
 		System.out.println("aaa...");//edit by edit
 		System.out.println("bbb...");
 	}
